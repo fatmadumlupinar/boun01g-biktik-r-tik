4170f30b6b62c2c97553308163dcf0c6f9f89547
irmakdai <69152099+irmakdai@users.noreply.github.com>
2020-08-30 20:44:22 +0300

    Update Shiny App.R

--- Shiny App.R
+++ Shiny App.R
@@ -1,11 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 pti <- c("shiny","tidyverse","leaflet","knitr","markdown", "png", "grid","tm", "SnowballC", "wordcloud","RColorBrewer","rio","lubridate")
 pti <- pti[!(pti %in% installed.packages())]
@@ -13,9 +5,7 @@ if(length(pti)>0){
     install.packages(pti)
 }
 
-##########
-### Shiny starter code
-##########
+
 library(shiny)
 library(tidyverse)
 library(leaflet)
